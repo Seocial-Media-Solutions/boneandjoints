f97d7bf6ffb226b06683838b20d7f3e36c8c32c6
Dilkhush4707 <155545584+Dilkhush4707@users.noreply.github.com>
2026-03-06 14:05:44 +0530

    blog  push  on   06/03/2025

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -143,6 +143,7 @@ export default async function sitemap() {
     "/blog/post-fracture-recovery-tips-orthopedic-guide-ajmer",
     "/blog/how-to-speed-up-bone-healing-after-fracture-expert-tips-ajmer",
     "/blog/spine-specialist-back-pain-slip-disc-treatment-ajmer-dr-abhishek-saxena",
+    "/blog/best-exercises-strong-bones-healthy-joints",
   ].map((path) => ({
     url: `${baseUrl}${path}`,
     lastModified: currentDate,
